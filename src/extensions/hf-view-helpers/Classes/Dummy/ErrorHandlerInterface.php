60367807cb47ddef98e31ebed181bdcfb619b0cc
Sinian Zhang <zhangsinian@hotmail.com>
2026-06-02 22:25:24 +0200

    [TASK] packages for unit test and phpstan

--- src/extensions/hf-view-helpers/Classes/Dummy/ErrorHandlerInterface.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Hausformat\ViewHelpers\Dummy;
-
-interface ErrorHandlerInterface
-{
-    public function handle(\Exception $e): void;
-}
--- src/extensions/hf-view-helpers/Classes/Dummy/ServiceInterface.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Hausformat\ViewHelpers\Dummy;
-
-interface ServiceInterface
-{
-    public function doSomething(): void;
-}
