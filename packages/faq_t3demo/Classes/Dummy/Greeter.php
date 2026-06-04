6f9134a20cbae3a5cc4933da73ea5adb8301665a
Sinian Zhang <zhangsinian@hotmail.com>
2026-06-04 12:45:22 +0200

    [TASK] Dummy in faq_t3demo

---
+++ packages/faq_t3demo/Classes/Dummy/Greeter.php
@@ -0,0 +1,26 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace B13\FaqT3demo\Dummy;
+
+final class Greeter
+{
+    public function greet(string $name): string
+    {
+        return 'Hello, ' . $name . '!';
+    }
+
+    public function greetWithTimeOfDay(string $name, int $hour): string
+    {
+        if ($hour < 12) {
+            return 'Good morning, ' . $name . '!';
+        }
+
+        if ($hour < 18) {
+            return 'Good afternoon, ' . $name . '!';
+        }
+
+        return 'Good evening, ' . $name . '!';
+    }
+}
